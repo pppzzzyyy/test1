070ca9997ffd44c4815c37af6cc0b1fee5e2fa92
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-07 18:10:42 +0800

    添加：  web的初始化

---
+++ src/main/java/com/example/test1/service/impl/TestServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.test1.service.impl;
+
+import com.example.test1.service.TestService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestServiceImpl implements TestService {
+}
