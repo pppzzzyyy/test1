4e9602424a9e293473ebc7306cc2339988eadca6
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-07 20:09:57 +0800

    添加：  测试调用

--- src/main/java/com/example/test1/controller/TestController.java
+++ src/main/java/com/example/test1/controller/TestController.java
@@ -1,8 +1,15 @@
 package com.example.test1.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.example.test1.entity.JdUser;
+import com.example.test1.mapper.JdUserMapper;
+import com.example.test1.service.JdUserService;
 import com.example.test1.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author : panzhenye
@@ -13,4 +20,19 @@ public class TestController {
 
     @Autowired
     private TestService testService;
+
+    @Autowired
+    private JdUserService jdUserService;
+
+    @Autowired
+    private JdUserMapper jdUserMapper;
+
+    @RequestMapping("test1")
+    public String test1(){
+        List<JdUser> list = jdUserService.list();
+        System.out.println("list = " + list);
+        List<JdUser> jdUsers = jdUserMapper.selectList(Wrappers.emptyWrapper());
+        System.out.println("jdUsers = " + jdUsers);
+        return "success";
+    }
 }
--- src/main/java/com/example/test1/mapper/JdUserMapper.java
+++ src/main/java/com/example/test1/mapper/JdUserMapper.java
@@ -2,10 +2,12 @@ package com.example.test1.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.test1.entity.JdUser;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author : panzhenye
  * @date : 2022/9/7 19:49
  */
+@Repository
 public interface JdUserMapper extends BaseMapper<JdUser> {
 }
