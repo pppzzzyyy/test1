a4a4d077526360d1c5b7f7dd4ca3a23bc3ad8b50
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-07 19:40:37 +0800

    添加：  实体类：   JdUser

---
+++ src/main/java/com/example/test1/entity/JdUser.java
@@ -0,0 +1,16 @@
+package com.example.test1.entity;
+
+import lombok.Data;
+
+/**
+ * @author : panzhenye
+ * @date : 2022/9/7 19:38
+ */
+@Data
+public class JdUser {
+    private String jdUserId;
+    private String jdUserName;
+    private String jdUserText1;
+    private String jdUserText2;
+    private String jdUserText3;
+}
