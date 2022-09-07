61ccb12ee8dc6d511360e741cdeecc4bea0a7afd
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-07 20:18:21 +0800

    添加：  MapperScan

--- src/main/java/com/example/test1/Test1Application.java
+++ src/main/java/com/example/test1/Test1Application.java
@@ -1,5 +1,6 @@
 package com.example.test1;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date : 2022/9/7 18:14
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.test1.mapper")
 public class Test1Application {
 
     public static void main(String[] args) {
