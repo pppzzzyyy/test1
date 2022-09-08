89ee5fc56cb27e7f590d5d3b80e073aea22a801d
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-08 15:44:25 +0800

    添加：  test1()初始测试方法

--- src/main/java/com/example/test1/controller/TestController.java
+++ src/main/java/com/example/test1/controller/TestController.java
@@ -2,6 +2,7 @@ package com.example.test1.controller;
 
 import com.example.test1.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,4 +14,10 @@ public class TestController {
 
     @Autowired
     private TestService testService;
+
+    @RequestMapping("test1")
+    public String test1(){
+
+        return "success";
+    }
 }
