a32e7a04216f0503521fd348211bd9916c471b5d
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-19 12:33:04 +0800

    新增：  redis锁

--- src/main/java/com/example/test1/service/impl/TestServiceImpl.java
+++ src/main/java/com/example/test1/service/impl/TestServiceImpl.java
@@ -3,9 +3,12 @@ package com.example.test1.service.impl;
 import com.example.test1.service.TestService;
 import com.example.test1.utils.Constant;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -16,44 +19,44 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class TestServiceImpl implements TestService {
     public static int a = 0;
-    public static ConcurrentHashMap<String,String> map = new ConcurrentHashMap<>();
+    public static ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
 //    public static HashMap<String,String> map = new HashMap<>();
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Override
     @Async
     public void asyncTest() {
-        while (map.putIfAbsent("a", "a") != null) {
-            log.info(Thread.currentThread().getName()+"没拿到锁");
+        while (!redisTemplate.opsForValue().setIfAbsent("a","a", Duration.ofSeconds(1))) {
+            log.info(Thread.currentThread().getName() + "没拿到锁");
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        log.info(Thread.currentThread().getName()+"拿到锁");
-//        synchronized (TestServiceImpl.class) {
-            if (Constant.b.get("a") <= 0) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Constant.b.computeIfPresent("a", (key, value) -> value + 100);
-            } else {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Constant.b.computeIfPresent("a", (key, value) -> value - 100);
+        log.info(Thread.currentThread().getName() + "拿到锁");
+        if (Constant.b.get("a") <= 0) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            Constant.b.computeIfPresent("a", (key, value) -> value + 100);
+        } else {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Constant.b.computeIfPresent("a", (key, value) -> value - 100);
+        }
 //            if (true) {
 //                throw new RuntimeException("2");
 //            }
-//        }
         log.info("inta = " + Constant.b.get("a"));
-        map.remove("a");
+        redisTemplate.delete("a");
     }
 
 }
