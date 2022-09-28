605f443cd117404c584553e15d1db566e7f6321a
ext.panzhenye1 <ext.panzhenye1@jd.com>
2022-09-28 12:09:17 +0800

    新增：  搭建eureka-server

---
+++ eureka-server/src/main/java/com/jd/test1/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.jd.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author : panzhenye
+ * @date : 2022/9/28 11:23
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
